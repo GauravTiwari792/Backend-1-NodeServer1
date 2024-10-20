d9de061f88bbe6a0b5a7573e71d63636059ae70b
GauravTiwari792 <gtiwarid792@gmail.com>
2024-10-20 14:47:25 +0530

    Nodejs Basics: Server 1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const http = require('http')
+// CommonJS Module
+// const http = require('http')
+// const {sum, difference} = require('./modules/calculate.js')
+
+// ES6 Module
+import http from 'http';
+import { sum, difference } from './modules/calculate.js';
 
 const server = http.createServer((req, res) => {
+    console.log(sum(2, 3))
+    console.log(difference(10, 5))
     res.end('BACKEND :)')
 })
 
---
+++ modules/calculate.js
@@ -0,0 +1,13 @@
+const sum = (num1, num2) => {
+    return num1 + num2
+}
+
+const difference = (num1, num2) => {
+    return num1 - num2
+}
+
+// CommonJS
+// module.exports = { sum, difference }
+
+// ES6
+export {sum, difference};
